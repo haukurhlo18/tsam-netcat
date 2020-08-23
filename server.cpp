d6072413c704a6c621149736fed35d7e817ff471
Haukur Hlöðversson <haukur@haukurh.is>
2020-08-23 15:47:23 +0000

    reindent file

--- server.cpp
+++ server.cpp
@@ -39,11 +39,11 @@
 // Client(int socket) - socket to send/receive traffic from client.
 class Client
 {
-  public:
+public:
     int sock;              // socket of client connection
     std::string name;      // Limit length of name of client's user
 
-    Client(int socket) : sock(socket){} 
+    Client(int socket) : sock(socket){}
 
     ~Client(){}            // Virtual destructor defined for base class
 };
@@ -63,62 +63,62 @@ std::map<int, Client*> clients; // Lookup table for per Client information
 
 int open_socket(int portno)
 {
-   struct sockaddr_in sk_addr;   // address settings for bind()
-   int sock;                     // socket opened for this port
-   int set = 1;                  // for setsockopt
+    struct sockaddr_in sk_addr;   // address settings for bind()
+    int sock;                     // socket opened for this port
+    int set = 1;                  // for setsockopt
 
-   // Create socket for connection. Note: OSX doesn´t support SOCK_NONBLOCK
-   // so we have to use a fcntl (file control) command there instead. 
+    // Create socket for connection. Note: OSX doesn´t support SOCK_NONBLOCK
+    // so we have to use a fcntl (file control) command there instead.
 
-   #ifndef SOCK_NONBLOCK
-      if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+#ifndef SOCK_NONBLOCK
+    if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+        perror("Failed to open socket");
+        return(-1);
+    }
+
+    int flags = fcntl(sock, F_GETFL, 0);
+
+    if(fcntl(sock, F_SETFL, flags | O_NONBLOCK) < 0)
+    {
+        perror("Failed to set O_NONBLOCK");
+    }
+#else
+    if((sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK , IPPROTO_TCP)) < 0)
       {
          perror("Failed to open socket");
          return(-1);
       }
+#endif
 
-      int flags = fcntl(sock, F_GETFL, 0);
+    // Turn on SO_REUSEADDR to allow socket to be quickly reused after
+    // program exit.
 
-      if(fcntl(sock, F_SETFL, flags | O_NONBLOCK) < 0)
-      {
-         perror("Failed to set O_NONBLOCK");
-      }
-   #else
-      if((sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK , IPPROTO_TCP)) < 0)
-      {
-         perror("Failed to open socket");
-         return(-1);
-      }
-    #endif
+    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &set, sizeof(set)) < 0)
+    {
+        perror("Failed to set SO_REUSEADDR:");
+    }
 
-   // Turn on SO_REUSEADDR to allow socket to be quickly reused after 
-   // program exit.
+    // Initialise memory
+    memset(&sk_addr, 0, sizeof(sk_addr));
 
-   if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &set, sizeof(set)) < 0)
-   {
-      perror("Failed to set SO_REUSEADDR:");
-   }
+    // Set type of connection
 
-   // Initialise memory
-   memset(&sk_addr, 0, sizeof(sk_addr));
+    sk_addr.sin_family      = AF_INET;
+    sk_addr.sin_addr.s_addr = INADDR_ANY;
+    sk_addr.sin_port        = htons(portno);
 
-   // Set type of connection
+    // Bind to socket to listen for connections from clients
 
-   sk_addr.sin_family      = AF_INET;
-   sk_addr.sin_addr.s_addr = INADDR_ANY;
-   sk_addr.sin_port        = htons(portno);
-
-   // Bind to socket to listen for connections from clients
-
-   if(bind(sock, (struct sockaddr *)&sk_addr, sizeof(sk_addr)) < 0)
-   {
-      perror("Failed to bind to socket:");
-      return(-1);
-   }
-   else
-   {
-      return(sock);
-   }
+    if(bind(sock, (struct sockaddr *)&sk_addr, sizeof(sk_addr)) < 0)
+    {
+        perror("Failed to bind to socket:");
+        return(-1);
+    }
+    else
+    {
+        return(sock);
+    }
 }
 
 // Close a client's connection, remove it from the client list, and
@@ -126,49 +126,49 @@ int open_socket(int portno)
 
 void closeClient(int clientSocket, fd_set *openSockets, int *maxfds)
 {
-     close(clientSocket);      
+    close(clientSocket);
 
-     // If this client's socket is maxfds then the next lowest
-     // one has to be determined. Socket fd's can be reused by the Kernel,
-     // so there aren't any nice ways to do this.
+    // If this client's socket is maxfds then the next lowest
+    // one has to be determined. Socket fd's can be reused by the Kernel,
+    // so there aren't any nice ways to do this.
 
-     if(*maxfds == clientSocket)
-     {
+    if(*maxfds == clientSocket)
+    {
         for(auto const& p : clients)
         {
             *maxfds = std::max(*maxfds, p.second->sock);
         }
-     }
+    }
 
-     // And remove from the list of open sockets.
+    // And remove from the list of open sockets.
 
-     FD_CLR(clientSocket, openSockets);
+    FD_CLR(clientSocket, openSockets);
 }
 
 // Process any message received from client on the server
 
-void clientCommand(int clientSocket, fd_set *openSockets, int *maxfds, 
-                  char *buffer) 
+void clientCommand(int clientSocket, fd_set *openSockets, int *maxfds,
+                   char *buffer)
 {
-  std::vector<std::string> tokens;     // List of tokens in command from client
-  std::string token;                   // individual token being parsed
+    std::vector<std::string> tokens;     // List of tokens in command from client
+    std::string token;                   // individual token being parsed
 
-  // Split command from client into tokens for parsing
-  std::stringstream stream(buffer);
+    // Split command from client into tokens for parsing
+    std::stringstream stream(buffer);
 
-  // By storing them as a vector - tokens[0] is first word in string
-  while(stream >> token)
-      tokens.push_back(token);
+    // By storing them as a vector - tokens[0] is first word in string
+    while(stream >> token)
+        tokens.push_back(token);
 
-  // This assumes that the supplied command has no parameters
-  if((tokens[0].compare("SYS") == 0) && (tokens.size() >= 2))
-  {
-     system(tokens[1].c_str());
-  }
-  else
-  {
-      std::cout << "Unknown command from client:" << buffer << std::endl;
-  }
+    // This assumes that the supplied command has no parameters
+    if((tokens[0].compare("SYS") == 0) && (tokens.size() >= 2))
+    {
+        system(tokens[1].c_str());
+    }
+    else
+    {
+        std::cout << "Unknown command from client:" << buffer << std::endl;
+    }
 }
 
 int main(int argc, char* argv[])
@@ -194,7 +194,7 @@ int main(int argc, char* argv[])
 
     // Setup socket for server to listen to
 
-    listenSock = open_socket(atoi(argv[1])); 
+    listenSock = open_socket(atoi(argv[1]));
 
     printf("Listening on port: %d\n", atoi(argv[1]));
     printf("       (range of available ports on skel.ru.is is 4000-4100)\n");
@@ -204,8 +204,8 @@ int main(int argc, char* argv[])
         printf("Listen failed on port %s\n", argv[1]);
         exit(0);
     }
-    else 
-    // Add the listen socket to socket set
+    else
+        // Add the listen socket to socket set
     {
         FD_SET(listenSock, &openSockets);
         maxfds = listenSock;
@@ -231,49 +231,49 @@ int main(int argc, char* argv[])
             // Accept  any new connections to the server
             if(FD_ISSET(listenSock, &readSockets))
             {
-               clientSock = accept(listenSock, (struct sockaddr *)&client,
-                                   &clientLen);
+                clientSock = accept(listenSock, (struct sockaddr *)&client,
+                                    &clientLen);
 
-               FD_SET(clientSock, &openSockets);
-               maxfds = std::max(maxfds, clientSock);
+                FD_SET(clientSock, &openSockets);
+                maxfds = std::max(maxfds, clientSock);
 
-               clients[clientSock] = new Client(clientSock);
-               n--;
+                clients[clientSock] = new Client(clientSock);
+                n--;
 
-               printf("Client connected on server\n");
+                printf("Client connected on server\n");
             }
             // Check for commands from already connected clients
             while(n-- > 0)
             {
-               for(auto const& pair : clients)
-               {
-                  Client *client = pair.second;
+                for(auto const& pair : clients)
+                {
+                    Client *client = pair.second;
 
-                  if(FD_ISSET(client->sock, &readSockets))
-                  {
-                      if(recv(client->sock, buffer, sizeof(buffer), MSG_DONTWAIT) == 0)
-                      {
-                          printf("Client closed connection: %d", client->sock);
+                    if(FD_ISSET(client->sock, &readSockets))
+                    {
+                        if(recv(client->sock, buffer, sizeof(buffer), MSG_DONTWAIT) == 0)
+                        {
+                            printf("Client closed connection: %d", client->sock);
 
-                          closeClient(client->sock, &openSockets, &maxfds);
-                          clientSocketsToClear.push_back(client->sock);
+                            closeClient(client->sock, &openSockets, &maxfds);
+                            clientSocketsToClear.push_back(client->sock);
 
-                      }
-                      else
-                      {
-                          std::cout << buffer << std::endl;
-                          clientCommand(client->sock, &openSockets, &maxfds, 
-                                        buffer);
-                      }
-                  }
-               }
+                        }
+                        else
+                        {
+                            std::cout << buffer << std::endl;
+                            clientCommand(client->sock, &openSockets, &maxfds,
+                                          buffer);
+                        }
+                    }
+                }
 
-               // Remove client from the clients list. This has to be done 
-               // out of the main loop, since we can't modify the iterator.
-               for(auto const& i : clientSocketsToClear)
-               {
-                   clients.erase(i);
-               }
+                // Remove client from the clients list. This has to be done
+                // out of the main loop, since we can't modify the iterator.
+                for(auto const& i : clientSocketsToClear)
+                {
+                    clients.erase(i);
+                }
             }
         }
     }
