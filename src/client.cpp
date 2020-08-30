e6e0d2c527469946f938f0967468a8da99d04a43
Haukur Hlöðversson <haukur@haukurh.is>
2020-08-30 21:48:58 +0000

    major refactor of client.cpp
    built class for client
    
    recevier in it's own thread

--- src/client.cpp
+++ src/client.cpp
@@ -23,79 +23,148 @@
 #include <thread>
 #include <map>
 
-void func(int sockfd)
-{
-    bool finished = false;
+class Client {
+public:
+
+  Client(char* hostname, char* port) {
+    this->hostname = hostname;
+    this->socket = this->build_socket(hostname, port);
+  }
+
+  /**
+   * Getter for socket file descriptor
+   *
+   * @return socket file descriptor
+   */
+  int socket_fd() {
+    return this->socket;
+  }
+
+  /**
+   * "start" the client,
+   * i.e. wait for input and send messages from stdin through socket.
+   * this will also spawn a new thread which receives messages the socket.
+   */
+  void start() {
+    std::thread receiver_thread([&]{
+      this->receiver();
+    });
     char buffer[1024];
     int n;
-    while (!finished) {
-        n = 4;
-        bzero(buffer, sizeof(buffer));
-
-        buffer[0] = buffer[2] = 'S';
-        buffer[1] = 'Y';
-        buffer[3] = ' ';
-        printf("anonymous@server $ ");
-        while ((buffer[n++] = getchar()) != '\n') continue;
-        if (strncmp(buffer, "SYS exit", 8) == 0 || strncmp(buffer, "SYS logout", 8) == 0) {
-            printf("Client Exit...\n");
-            finished = true;
-            continue;
-        }
-        write(sockfd, buffer, sizeof(buffer));
-        bzero(buffer, sizeof(buffer));
-        read(sockfd, buffer, sizeof(buffer));
-        printf("%s", buffer);
-
-        bzero(buffer, sizeof(buffer));
-//        std::cout << std::endl;
+    while (!this->stop) {
+      n = 0;
+      bzero(buffer, sizeof(buffer));
+      while ((buffer[n++] = getchar()) != '\n') continue;
+      if (strncmp(buffer, "exit", 4) == 0 || strncmp(buffer, "logout", 5) == 0) {
+        printf("Client Exit...\n");
+        this->close();
+        receiver_thread.join();
+        continue;
+      }
+      send(this->socket, buffer, sizeof(buffer), 0);
+      bzero(buffer, sizeof(buffer));
     }
-}
+  }
 
-int main(int argc, char *argv[])
-{
-    // socket_fd = socket file descriptor
-    int socket_fd, port;
-    struct sockaddr_in serv_addr;
+  /**
+   * destructor for client,
+   * here we run our "exit" procedure
+   */
+  ~Client() {
+    this->close();
+  }
 
-    struct addrinfo hints = {0}, *address_info;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
+protected:
+  int socket;
+  char* hostname;
+  bool stop = false;
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <hostname> port\n", argv[0]);
+  /**
+   * set stop flag to true, for other processes
+   * close the socket
+   */
+  void close() {
+    this->stop = true;
+    ::close(this->socket);
+  }
+
+  /**
+   * Start the receiver, which print out messages it receives from the socket.
+   * ideal to run in it's own thread, so it's non-blocking
+   *
+   */
+  void receiver() {
+    int re;
+    char buffer[64];
+    bzero(buffer, sizeof(buffer));
+    while (!this->stop) {
+      re = recv(this->socket, buffer, sizeof(buffer) - 1, MSG_FLUSH);
+      if (re < 1 && !this->stop) {
+        fprintf(stderr, "Lost connection to server (%s)\n", this->hostname);
+        this->close();
         exit(1);
+      }
+      printf("%s", buffer);
+      bzero(buffer, sizeof(buffer));
     }
+  }
 
-    const int status = getaddrinfo(argv[1], argv[2], &hints, &address_info);
-    if (status != 0) {
-        fprintf(stderr, "%s: %s\n", argv[1], gai_strerror(status));
-        exit(EXIT_FAILURE);
+  /**
+   * Resolves hostname and establishes connection to host via socket
+   * Inspiration from getaddrinfo manual page
+   *
+   * @param hostname
+   * @param port
+   * @return socket file descriptor
+   */
+  int build_socket(char* hostname, char* port) {
+    struct addrinfo hints, *res, *res0;
+    int error;
+    int s;
+    const char *cause = NULL;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    error = getaddrinfo(hostname, port, &hints, &res0);
+    if (error) {
+      fprintf(stderr, "%s: %s", hostname, gai_strerror(error));
+      exit(1);
     }
+    s = -1;
+    for (res = res0; res; res = res->ai_next) {
+      s = ::socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+      if (s < 0) {
+        cause = "ERROR opening socket";
+        continue;
+      }
 
-    port = atoi(argv[2]);
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+      if (connect(s, res->ai_addr, res->ai_addrlen) < 0) {
+        cause = "ERROR connecting";
+        ::close(s);
+        s = -1;
+        continue;
+      }
 
-    if (socket_fd < 0) {
-        perror("ERROR opening socket");
-        exit(EXIT_FAILURE);
+      break;  /* okay we got one */
     }
-
-    struct sockaddr_in *addr;
-    addr = (struct sockaddr_in *)address_info->ai_addr;
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-    serv_addr.sin_addr = (struct in_addr)addr->sin_addr;
-
-    if (connect(socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR connecting");
-        exit(EXIT_FAILURE);
+    if (s < 0) {
+      fprintf(stderr, "%s", cause);
+      exit(1);
     }
+    freeaddrinfo(res0);
+    return s;
+  }
+};
 
-    func(socket_fd);
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <hostname> port\n", argv[0]);
+    exit(1);
+  }
 
-    close(socket_fd);
-    return 0;
+  Client client(argv[1], argv[2]);
+  client.start();
+
+  return 0;
 }
